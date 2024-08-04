0559572425b12c9dc48da53e8d50f2a4896cea17
Bernardino Antonius Kase <reynaldokase.rk@gmail.com>
2024-08-04 23:47:37 +0800

    fix: fix and update feedforward function with documentation

--- training/fpaff_model.js
+++ training/fpaff_model.js
@@ -1,7 +1,7 @@
 function fpaDemo(para = [20, 0.8]) {
     const n = para[0];  // ukuran populasi
     const p = para[1];  // switch Probability 
-    const N_iter = 3000;  // Iterasi
+    const N_iter = 100;  // Iterasi
 
     const dataTraining = [
         [1,0,0,1],
@@ -85,7 +85,7 @@ function fpaDemo(para = [20, 0.8]) {
     for (let i = 0; i < n; i++) {
         Sol[i] = Lb.map((lb, index) => lb + (Ub[index] - lb) * Math.random()); 
         console.log(`nilai Solusi ke ${i+1} = ${Sol[i]}`); // Sol ke i merupakan vektor solusi atau solusi pada populasi ke n
-        Fitness[i] = feedforward(dataTraining[0], Sol[i], hidden, output); // Fitness  ke i merupakan nilai fitness min pada vektor solusi atau pada solusi pada populasi ke n
+        Fitness[i] = Fun(dataTraining[0], Sol[i], hidden, output); // Fitness  ke i merupakan nilai fitness min pada vektor solusi atau pada solusi pada populasi ke n
         // console.log(`nilai fitnes ke ${i+1} = ${Fitness[i]}`);
     }
     
@@ -106,8 +106,8 @@ function fpaDemo(para = [20, 0.8]) {
     // variabel S sama seperti nilai pada variabel Sol dimana akan menampung seluruh nilai vektor solusi atau solusi pada populasi ke n 
     // namun fungsi variabel S dipakai saat pengerjaan FPA untuk menampung nilai solusi baru pada proses global atau lokal penyerbukan
     let S = [...Sol];
-    console.log('S = ')
-    console.log(S)
+    // console.log('S = ')
+    // console.log(S)
     // console.log(`\n Solusi: ${S}\n========================================================================================================================================`);
     // console.log(`\n\n================================================ KERJAKAN FPA ================================================`)
     for (let t = 0; t < N_iter; t++) {
@@ -115,12 +115,12 @@ function fpaDemo(para = [20, 0.8]) {
         for (let i = 0; i < n; i++) {
             console.log(`                                                       POPULASI ${i+1}`)
             // console.log(`\niterasi ${t+1} --- populasi ${i+1}`);
-            console.log(`nilai Sol-${i+1}`)
-            console.log(Sol[i])
+            // console.log(`nilai Sol-${i+1}`)
+            // console.log(Sol[i])
             let randomP = Math.random();
             // console.log(`\nnilai random p = ${randomP}`);
             if (randomP < p) { // satu populasi hanya menggunakan 1x random p
-                console.log(`PENYERBUKAN GLOBAL`) 
+                // console.log(`PENYERBUKAN GLOBAL`) 
                 // console.log(`\nnilai random p = ${randomP}`);
                 let L = Levy(d);
                 // console.log(`\nNilai Levy Flight ke ${t}, ${i}: ${L}`)
@@ -135,35 +135,43 @@ function fpaDemo(para = [20, 0.8]) {
                 // console.log(Sol[i])
                 // console.log(Sol[i])
             } else {
-                console.log('|||PENYERBUKAN LOKAL|||')
+                // console.log('|||PENYERBUKAN LOKAL|||')
                 let epsilon = Math.random();
                 let JK = shuffle([...Array(n).keys()]);
                 S[i] = S[i].map((s, index) => s + epsilon * (Sol[JK[0]][index] - Sol[JK[1]][index]));
                 S[i] = simpleBounds(S[i], Lb, Ub);
                 // console.log(`\nLocal Pollination\nNilai Sol[${t}, ${i}] = ${S[i]}`);
             }
-            console.log(`hasil nilai S-${i+1}`)
+            console.log(`nilai solusi terbaru hasil penyerbukan = `)
+            // console.log(`hasil nilai S-${i+1}`)
             console.log(S[i])
             let FnewFF = []; // variabel FnewFF akan menampung hasil proses FF. solusi hasil proses penyerbukan akan di proses dengan data yang ada
             for (let j = 0; j < dataTraining.length; j++) {
                 // console.log(`data ke ${j+1}`)
-                FnewFF[j] = feedforward(dataTraining[j], S[i], hidden, output);
-                console.log(`data ${j+1} >>> nilai Fnew = ${FnewFF}`)
+                FnewFF[j] = Fun(dataTraining[j], S[i], hidden, output);
+                // console.log(`data ${j+1} >>> nilai Fnew = ${FnewFF}`)
             }
-            console.log(FnewFF)
+            // console.log(FnewFF)
             let Fnew = Math.min(...FnewFF)
-            console.log(`nilai Fnew = ${Fnew}`)
+            // console.log(`nilai Fnew = ${Fnew}`)
             
-            asdfs
             // console.log(`----------------------------------------------------------------`)
             // console.log(`nilai Fitnes [${t}, ${i}] = ${Fitness[i]}`);
             // console.log(`nilai Fnew [${t}, ${i}] = ${Fnew}`);
             // console.log(`nilai Fmin [${t}, ${i}] = ${fmin}`);
             // console.log(`----------------------------------------------------------------`)
-            if (Fnew <= Fitness[i]) {
+            // console.log(`nilai Fitness-${i+1} = ${Fitness[i]}`)
+            
+            // update Sol [i] dan Fitness [i], jika nilai Fnew pada populasi sekarang kurang dari Fitness ke i pada populasi sebelumnya
+            if (Fnew <= Fitness[i]) { 
                 // console.log(`\nEvaluasi solusi baru\n Fnew <= Fitness?\n Fitness == Fnew`)
                 Sol[i] = [...S[i]];
                 Fitness[i] = Fnew;
+
+                // console.log(`nilai Sol[${i+1}] =`)
+                // console.log(Sol[i])
+                // console.log(`niali Fitness [${i+1}] = ${Fnew}`)
+                // asdsad
                 // console.log(`nilai Fnew [${t}, ${i}] = ${Fnew}`);
                 // console.log(`nilai Sol [${t}, ${i}] = ${Sol[i]}`);
                 // console.log(`nilai new [${t}, ${i}] = ${Fitness[i]}`);
@@ -171,13 +179,19 @@ function fpaDemo(para = [20, 0.8]) {
             // console.log(`----------------------------------------------------------------`)
             // console.log(`\nnilai Fitnes min ${fmin}`);
             // console.log(`----------------------------------------------------------------`)
-            if (Fnew <= fmin) {
+
+            // update best dan fmin, jika Fnew pada populasi sekarang kurang dari fmin pada iterasi sebelumnya
+            if (Fnew <= fmin) { 
                 // console.log(`perbarui global best\n Fnew <= fmin?\n fmin == Fnew`)
                 best = [...S[i]];
                 fmin = Fnew;
                 // console.log(`nilai best [${t}, ${i}] = ${best}`);
                 // console.log(`nilai Fnew [${t}, ${i}] = ${Fnew}`);
             }
+            console.log(`nilai fmin = ${fmin}`);
+            console.log(`best = `)
+            console.log(best)
+            // console.log(`>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`)
             // console.log(`=============\nFmin = ${fmin}`);
 
             // console.log(Fitness.indexOf(fmin))
@@ -191,11 +205,13 @@ function fpaDemo(para = [20, 0.8]) {
         //     console.log(`Iteration: ${t}\nBest: ${best}\n
         //                         fmin: ${fmin}\n`);
         // }
-        console.log(`iterasi ${t+1}, fmin: ${fmin}\n`);
+        console.log(`iterasi ${t+1}, fmin: ${fmin}\nbset = `);
+        console.log(best)
+        // asdfsdadf
     }
 
     console.log(`Total number of evaluations: ${N_iter * n}`);
-    console.log(`Best solution: ${best}, fmin: ${fmin}`);
+    console.log(`Best solution: ${best}\n fmin: ${fmin}`);
     return { best, fmin, N_iter };
 }
 
@@ -217,10 +233,9 @@ function sigmoid(x) {
     return 1 / (1 + Math.exp(-x));
 }
 
-function feedforward(data, solusi, hidden, output ) {
+function Fun(data, solusi, hidden, output ) {
     // console.log('------------------------KERJAKAN FEEDFORWARD------------------------')
     let _solution = solusi;
-    // console.log(_solution)
     let _dtTraining = data;
     const _bias = 1;
     const _input = _dtTraining.length; //mendapatkan nilai jumlah nilai inputan dalam data training
@@ -238,95 +253,75 @@ function feedforward(data, solusi, hidden, output ) {
     const _biasToOuput = _bias * _output;
     const _hiddenToOutput = _hidden * _output;
     const _hiddenAndBiasToOuput = _biasToOuput + _hiddenToOutput;
-    const dimension = _inputAndBiasToHidden + _hiddenAndBiasToOuput;
+    const dimension = _solution.length;
+    console.log(`jumlah dimensi = ${dimension}`)
     
-    // console.log(`
-    //     input = ${_input}
-    //     hidden = ${_hidden}
-    //     output = ${_output}
-    //     bias ke hidden ${_biasToHidden}
-    //     input ke hidden ${_inputToHidden}
-    //     jumlah bias dan input ke hidden ${_inputAndBiasToHidden}
-    //     bias ke output ${_biasToOuput}
-    //     hidden ke output ${_hiddenToOutput}
-    //     jumlah bias dan hidden ke output ${_hiddenAndBiasToOuput}
-    //     dimensi = ${dimension}
-    //     `)
-    // jumlah dimensi pada jaringan MLP
-
-    //================================================================
     // total variabel solusi pada input dan bias ke hidden layer
     let _total_vsLayerOne = _solution.length - _hiddenAndBiasToOuput; //total input dan bias ke hidden layer
-    // console.log(`total layer di input = ${_total_vsLayerOne}`);
+
     // mendapatkan vektor solusi dari layer bagian pertama ke hidden layer
     let _vsLayerOne = []; //simpan vektor solusi sementara
     for (let i = 0; i < _total_vsLayerOne; i++) {
         _vsLayerOne.push(_solution[i]);
     }
+    console.log(`variabel solusi 1 = `)
+    console.log(_vsLayerOne);
+    
     // pisahkan terlebih dahulu bias ke hidden
     // sisa dari _vsLayerOne merupakan variabel solusi dari input ke hidden layer
     // let _biasToHidden = _total_vsLayerOne - _inputToHidden; //jumlah bias ke hidden layer == _biasToHidden
-    // vektor solusi input layer 
-    let _final_vs_inputToHidden = getInputWeight(_solution.slice(_biasToHidden, _total_vsLayerOne), _input); //vektor solusi input ke hidden
-    // console.log(`
-    //     total variabel solusi pada input dan bias ke hidden = ${_total_vsLayerOne}
-    //     variabel solusi input ke hidden = ${_vsLayerOne}
-    //     bias ke hidden = ${_biasToHidden}
-    //     variabel solusi input ke hidden = ${_final_vs_inputToHidden}
-    //     `);
+    // vektor solusi input layer ke hidden layer
+    let _vs_inputToHidden = getInputWeight(_solution.slice(_biasToHidden, _total_vsLayerOne), _input); //vektor solusi input ke hidden
     
     //================================================================
     // membagi vektor solusi layer pertama dan kedua
     let _vsLayerTwo = _solution.slice(_total_vsLayerOne);
-    let _total_vs_hiddenToOutput = _solution.length - _inputAndBiasToHidden - _hiddenToOutput; //jumlah vektor solusi hidden ke output layer
-    
-    // console.log(`hidden to output = ${_total_vs_hiddenToOutput}`);
+    console.log(`nilai vs 2 = ${_vsLayerTwo}`)
+    let _total_vs_biasToOutput = dimension - _inputAndBiasToHidden - _hiddenToOutput; //jumlah vektor solusi hidden ke output layer
     
     //memisahkan vektor solusi bias dan hidden layer dan
     //simpan vektor solusi bias ke dalam variabel
-    let _final_vs_biasToHidden = [];
-    for (let i = 0; i < _total_vs_hiddenToOutput; i++){
-        _final_vs_biasToHidden.push(_vsLayerTwo[i])
+    let _vs_biasToOutput = [];
+    for (let i = 0; i < _total_vs_biasToOutput; i++){
+        _vs_biasToOutput.push(_vsLayerTwo[i]) // vektor solusi bias ke output layer
     }
+    
+    let _vs_hiddenToOutput = getInputWeight(_solution.slice(_total_vsLayerOne + _total_vs_biasToOutput, solusi.length), hidden); // vektor solusi  hidden ke output
+    
+    let outputFeedForward = feedforward(hidden, data, _vsLayerOne, _vs_inputToHidden, output, _vsLayerTwo, _vs_hiddenToOutput)
+    console.log(`nilai output FF = ${outputFeedForward}`)
+    
+    let fmin = Math.min(...outputFeedForward);
+    // console.log(`fmin = ${fmin}`)
+    // asdasds
+    return fmin;
+}
 
-    let _final_vs_hiddenToOutput = getInputWeight(_solution.slice(_total_vsLayerOne + _total_vs_hiddenToOutput, solusi.length), hidden); // mendapatkan vektor solusi  hidden ke output
-    // console.log(`
-    //     variabel solusi hidden ke output = ${_vsLayerTwo}
-    //     total variabel solusi hidden ke output = ${_total_vs_hiddenToOutput}
-    //     variabel solusi hidden ke output = ${_final_vs_hiddenToOutput}
-    //     `)
-    // menjalankan fungsi aktivasi sebanyak jumlah hidden layer
-    // nilai bias akan ditambahkan dengan hasil kali antara nilai input dengan nilai antara bobot input ke hidden
+function feedforward(hiddenLayer, data, vektorSolusi1, vektorSolusiInputToHidden, outputLayer, vektorSolusi2, vektorSolusiHiddenToOutput){
+    // menghitung nilai fungsi aktivasi pada input layer ke hidden layer
     let _z_in = []
-    for (let i = 0; i < hidden; i++){
-        let activation1 = _vsLayerOne[i]
+    for (let i = 0; i < hiddenLayer; i++){
+        let activation1 = vektorSolusi1[i]
         for (let j = 0; j < data.length; j++){
             // console.log(j)
             // console.log(data[i][j])
-            activation1 += data[j] * _final_vs_inputToHidden[i][j]
+            activation1 += data[j] * vektorSolusiInputToHidden[i][j]
         }
         // console.log(activation1)
         // asdasdas
         _z_in.push(sigmoid(activation1));
+        
     }
-    // console.log(_z_in)
-
-    // menjalankan fungsi aktivasi sebanyak jumlah output layer
-    // nilai bias akan ditambahkan dengan hasil kali antara nilai aktivasi pada hidden layer
-    // dengan nilai bobot antara hidden ke output layer
+    // menghitung nilai fungsi aktivasi pada hidden layer ke output layer
     let _z = []; // menampung nilai aktivasi dari hidden ke output layer
-    for (let i = 0; i < output; i++){
-        let activation2 = _vsLayerTwo[i]
+    for (let i = 0; i < outputLayer; i++){
+        let activation2 = vektorSolusi2[i]
         for (let j = 0; j < _z_in.length; j++){ 
-            activation2 += _z_in[j] * _final_vs_hiddenToOutput[i][j]
+            activation2 += _z_in[j] * vektorSolusiHiddenToOutput[i][j]
         }
         _z.push(sigmoid(activation2))
     }
-    // console.log(_z)
-    // return _z;
-    
-    let fmin = Math.min(..._z);
-    return fmin;
+    return _z;
 }
 
 function getInputWeight(data, size) {
